3a4520d1f53d79ffcf321f39c71f0361c9624ad0
Iimanbek <imanbek.akylbekov2017@gmail.com>
2022-12-20 15:39:32 +0600

    done lay out

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,24 @@
-<script>
 
-</script>
 
 <template>
-  <main>
-    <h1>hello</h1>
+  
+    <navigation></navigation>
+    <v-main></v-main>
+    <v-footer></v-footer>
     <TheWelcome />
-  </main>
+  
 </template>
+
+<script>
+  import Header from '../components/Header.vue'
+  import Main from '../components/Main.vue'
+  import Footer from '../components/Footer.vue'
+
+  export default {
+    components:{
+      navigation:Header,
+      'v-main':Main,
+      'v-footer':Footer
+    }
+  }
+</script>
