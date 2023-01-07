fed4deb9582dd3846b3489b45ff7d5f42963b9ec
Iimanbek <imanbek.akylbekov2017@gmail.com>
2023-01-07 11:46:15 +0600

    done example

--- src/views/Example.vue
+++ src/views/Example.vue
@@ -1,5 +1,101 @@
 <template>
-    <BaseLayout>
-        <h1>hello</h1>
-    </BaseLayout>
+    <div>
+        <div class="nav-wrapper">
+            <nav>
+                <router-link to="/private">LOGO</router-link>
+                <span>about</span>
+                <span>Projects</span>
+                <span>Home</span>
+            </nav>
+        </div>
+        <main>
+            <div class="inner-main">
+                <h2>Hello world</h2>
+                <div class="p-wrap">
+                    <p v-for="n of 112">hello world</p>
+                </div>
+            </div>
+        </main>
+    </div>
+    <footer>
+        <div class="footer-inner-wrap">
+            <div class="colfooter" v-for="n of 3">
+                <h3>Obj</h3>
+                <span>Lorem, ipsum.</span>
+                <span>Lorem, ipsum.</span>
+                <span>Lorem, ipsum.</span>
+            </div>
+        </div>
+    </footer>
 </template>
+
+<script>
+
+export default {
+    name: 'Private',
+    
+}
+</script>
+
+<style scoped lang="scss">
+.inner-main{
+    padding-top: 100px ;
+}
+.footer-inner-wrap{
+    display: flex;
+    justify-content: space-around ;
+    background: #000;
+}
+.colfooter{
+    display: flex;
+    flex-direction: column;
+    gap: 10px ;
+    color: white ;
+    padding: 20px ;
+}
+main{
+    height: 80vh ;
+    max-width: 1300px ;
+    width: 100% ;
+    margin: auto ;
+    div{
+        h2{
+            text-align: center;
+        }
+    }
+}
+.nav-wrapper{
+    background: #000;
+    color: white;
+    position: fixed;
+    width: 100%;    
+
+}
+nav{
+    display: flex;
+    justify-content: space-between;
+    max-width: 1280px;
+    font-weight: 600;
+    padding: 20px 0;
+    width: 100%;
+    margin: 0 auto;
+}
+.text-wrapper{
+    max-width: 1300px;
+    max-height: 100vh;
+    height: 100%;
+    width: 100%;
+    margin: auto;
+    padding-top: 100px ;
+}
+
+a{
+    text-decoration: none ;
+    color: white  ;
+}
+.p-wrap{
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+}
+</style>
--- src/views/Private.vue
+++ src/views/Private.vue
@@ -1,32 +1,86 @@
 <template>
     <div>
-        <div>
+        <div class="nav-wrapper">
             <nav>
-                <span>LOGO</span>
+                <router-link to="/example">LOGO</router-link>
                 <span>about</span>
                 <span>Projects</span>
                 <span>Home</span>
             </nav>
         </div>
+        <div>
+            <div class="text-wrapper">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab quibusdam molestias necessitatibus totam aliquid ipsum laudantium labore reprehenderit eveniet fuga, perspiciatis voluptatibus eum obcaecati laborum deserunt recusandae iusto illum! Veritatis eos voluptatibus obcaecati dolore ratione? Soluta, consectetur explicabo! Mollitia eveniet voluptatum ipsum laboriosam fugit aut laborum aperiam blanditiis nostrum nihil harum, necessitatibus ullam ea velit, asperiores provident incidunt consectetur perferendis. Deserunt assumenda accusamus earum hic, sed optio reiciendis architecto molestiae unde! Earum impedit hic id nulla quasi dolorum fugiat. Quos architecto error laborum incidunt ipsum culpa quis vitae animi, molestias porro tenetur sed unde, iusto quam soluta ipsa vel! Vel quaerat aliquam iure! Porro, animi accusamus quasi ut unde veniam ad neque numquam illum alias minima. Iure illum incidunt libero odit ipsum amet praesentium reprehenderit harum vero beatae fugit, vel nihil ipsa voluptate eum nostrum ea, illo magni iste perferendis nemo, temporibus ab? Repellat doloremque praesentium nostrum nulla quasi quae et rem distinctio provident nisi, nemo aut voluptatem! Consequatur obcaecati, adipisci porro recusandae soluta ipsam quaerat quia ab saepe vero magnam sequi, unde nesciunt dolor dolorem magni enim! Rem voluptas veritatis soluta eum iusto, optio quo suscipit, delectus reprehenderit labore nostrum, distinctio saepe quaerat molestias dolorum voluptate esse vel aspernatur blanditiis assumenda architecto ea atque ab tempora. Asperiores consectetur autem voluptates. Ut hic facilis similique incidunt adipisci, atque voluptatem minima ducimus iure? Sequi, a quis? Deserunt veniam voluptates eveniet cum, neque accusamus blanditiis soluta perferendis voluptatum nesciunt ullam odio adipisci. Dignissimos vitae nesciunt quam minima magnam quia eos temporibus distinctio ad eius voluptas neque sint incidunt, labore eveniet odit architecto quas voluptatibus dolorum. Ullam totam incidunt laudantium, officia voluptatum consectetur quasi, qui soluta corrupti maiores facere illum! Possimus magnam commodi quo amet tenetur fugiat ab quaerat aliquid. Quam error cum assumenda odit eaque, facilis pariatur corrupti minus dolor eum, culpa doloribus ipsa explicabo nemo provident dolorem et numquam magni iusto consequuntur inventore nostrum dignissimos? Eveniet fuga quibusdam quidem reiciendis reprehenderit provident eius illo, nesciunt dolor hic ipsa accusantium consequuntur? Aliquid ducimus neque dolore dolores, aspernatur vel repellendus, libero saepe totam vero voluptates fuga doloremque velit dolor ea. Sint, atque voluptatem cum omnis, fugiat soluta vero amet, odio dicta quos sunt culpa? Ipsa eos ratione maiores qui nulla! Facilis praesentium et, vitae autem quae sequi placeat quia voluptate repellat laborum enim amet labore. Deserunt nemo quidem commodi blanditiis laborum, molestias corrupti architecto aut molestiae? Maxime, sapiente! Eum nobis labore voluptatem maxime autem doloribus maiores dolores excepturi distinctio magnam! Similique animi assumenda nulla explicabo cupiditate? Debitis accusantium atque vitae placeat porro est, odio veritatis consequatur sequi architecto, aspernatur labore distinctio dolores itaque impedit repellat quibusdam deserunt praesentium error quasi corrupti. Aliquam, laborum. Ea voluptas blanditiis sed deleniti vero aliquam itaque provident quia iste ad incidunt, inventore totam libero facere eum nisi! Ipsa quibusdam fuga autem suscipit distinctio enim, quas animi! Unde et ducimus cum asperiores earum eos quisquam praesentium obcaecati iusto dignissimos consequatur porro fugiat hic debitis maxime amet illo vero, ex doloribus. Adipisci optio, hic accusamus vitae eveniet harum minus amet quaerat deserunt voluptate alias praesentium nisi nam a at sapiente ratione provident odio, recusandae exercitationem possimus architecto animi blanditiis voluptatibus. Eligendi cumque, dignissimos assumenda doloribus, ipsam, odit perspiciatis quibusdam quae voluptatibus atque sunt deleniti perferendis voluptatum! Facilis, neque excepturi eum explicabo voluptatibus, qui rem odio necessitatibus eius fuga inventore iure dolorum blanditiis assumenda amet deleniti. Dignissimos quo accusamus optio mollitia deserunt doloremque architecto fugit illum dolores neque, aspernatur omnis, velit alias dolor fugiat provident aut rerum perferendis suscipit. Cupiditate quisquam ea non accusamus quia veritatis reprehenderit earum similique rem numquam! Saepe quod vero eos corporis at, similique placeat labore explicabo maiores fugiat veritatis iusto fugit, quibusdam voluptatibus numquam a magnam porro dolores rem dolor nam. Omnis esse soluta, eveniet repudiandae illo voluptates ratione suscipit totam magnam consectetur aspernatur quo aliquam, repellat beatae dicta sed reiciendis deleniti voluptatem laborum tempora vitae numquam ullam vel quae? Deserunt dicta hic repellat reprehenderit sint autem iusto officiis labore et! Harum dolor cumque ipsa, animi libero similique, dolorum rerum pariatur accusantium excepturi vitae itaque maxime, sapiente omnis sequi? Deserunt tenetur adipisci, dolorum molestiae, aliquid quaerat excepturi atque ipsa magni dolores id iure mollitia fuga repellendus! Dignissimos est laudantium nulla in necessitatibus eveniet eius? Error, rem corrupti id ex officiis similique officia dolorem eligendi commodi? Quisquam expedita sunt nemo, ab atque quaerat distinctio necessitatibus neque id ullam facere ipsum, amet eos laudantium impedit labore reiciendis perspiciatis fuga consequatur modi velit debitis minus, commodi aspernatur. Voluptatum veritatis enim soluta sed ut neque et ea distinctio, suscipit consectetur error eius nihil itaque doloremque, rerum ipsam quaerat officiis excepturi magnam unde voluptates accusamus, exercitationem earum. Ut dolorum minus numquam ipsum possimus est, quisquam vel voluptate qui maxime odio dolore nam quia cupiditate tempore! Iure facilis dicta fugit quis, accusamus deleniti eius dolorem, repudiandae sapiente maiores quidem facere numquam natus, nulla illum reprehenderit dolore quo magnam. Tempora at ipsum mollitia sequi aspernatur ex minus rerum quod odit odio deleniti assumenda, delectus eveniet ab. Porro voluptas autem, quisquam, distinctio ducimus reprehenderit earum, esse dignissimos ullam praesentium officia? Odio iusto repellat delectus omnis dolores ad deserunt enim incidunt quos, praesentium inventore id, dignissimos aspernatur sapiente error magnam quis exercitationem non nulla quod quia necessitatibus eveniet, asperiores minima. Optio rem expedita voluptatibus fugit quaerat vero, consequuntur dicta doloribus, quasi temporibus incidunt quo assumenda nobis nihil maxime dolorem possimus commodi? Non, harum. Assumenda expedita deleniti debitis corporis sequi et, hic molestias, voluptates at aliquid, quos autem earum nobis reprehenderit ipsam id! Aspernatur, quasi odit. Nulla nemo temporibus qui architecto porro soluta corporis optio voluptates corrupti? Repellat rerum exercitationem adipisci nostrum earum accusantium, doloremque ab atque? Cupiditate odio molestiae consectetur adipisci autem ducimus ab voluptatibus eaque sit distinctio facilis explicabo repellendus natus repellat hic labore ex, in qui corrupti exercitationem doloremque! Dolores vero quibusdam necessitatibus ab suscipit sint reprehenderit, eaque dolor cumque voluptatum rem optio repellat, adipisci, error itaque neque repellendus deserunt at corporis! Maxime ad expedita iste aut soluta doloribus, eveniet omnis nihil doloremque dolore aliquam temporibus dicta repellat, consequuntur, dignissimos rerum error asperiores!
+            </div>
+            <form>
+            <input class="inputt"/>
+            <button class="btn" type="submit">send</button>
+            </form>
+        </div>
     </div>
 </template>
 
-<script>
+<script>   
+
+
 export default {
     name: 'Private',
     data: () => ({
-        hello: 'hello world'
+        inputtext:'',
+        array: [ 
+            1, 2, 4, 5, 6, 7, 8, 9
+        ],
+        obj: {
+            name: 'Tom',
+            age: 19,
+            genrder: 'male',
+            nation: 'Asian'
+        },
     }),
 }
 </script>
 
 <style lang="scss">
+.nav-wrapper{
+    background: #000;
+    color: white;
+    position: fixed;
+    width: 100%;    
+
+}
 nav{
     display: flex;
     justify-content: space-between;
-    max-width: 1280px ;
+    max-width: 1280px;
+    font-weight: 600;
+    padding: 20px 0;
     width: 100%;
     margin: 0 auto;
-    
+}
+.text-wrapper{
+    max-width: 1300px;
+    height: 100%;
+    width: 100%;
+    margin: auto;
+    padding-top: 100px ;
+}
+a{
+    text-decoration: none ;
+    color: white  ;
+  }
+.inputt{
+    border: 1px solid black ;
+    margin-left: 100px ;
+}
+.btn{
+    border: 1px solid black;
+    background: black;
+    color: white ;
+    width: 50px;
+    margin-bottom: 100px ;
+    &:hover{
+        background: white;
+        color: black;       
+    }
 }
 </style>
