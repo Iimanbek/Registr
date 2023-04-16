8bb145771e013b159f828d329c24fd746bc3f926
Iimanbek <imanbek.akylbekov2017@gmail.com>
2023-04-16 23:57:35 +0600

    d

--- src/views/LogInView.vue
+++ src/views/LogInView.vue
@@ -1,32 +1,34 @@
 <template>
-      <div class="main-wrapper">
+    <div class="main-wrapper">
         <div class="form-wrap">
             <div class="nammm">
                 <h2>LOG IN</h2>
             </div>
             <div class="inp-wrap">
                 <label for="inp3">E-mail</label>
-                <input id="inp3" placeholder="" type="text" v-model="FormData.email" >
+                <input id="inp3" placeholder="" type="text" v-model="FormData.email">
                 <label for="inp4">Password</label>
                 <input id="inp4" placeholder="" v-model="FormData.password" :type="show1 ? 'text' : 'password'">
                 <!-- <button @click="show1 = !show1">show</button>
                  -->
-                 <div class="show">
-                     <input @change="show1 = !show1" class="chinp" type="checkbox" >
+                <div class="show">
+                    <input @change="show1 = !show1" class="chinp" type="checkbox">
                     <label>Show password</label>
-                 </div>
-                
+                </div>
+
             </div>
             <div class="btn-wrap">
                 <!-- <button class="btn1" @click="login" :disabled="email.length<=3 || password.length <= 5"> LOG IN </button> -->
-                <v-btn class="btn1" @click="login" :loading="loading1" :disabled="FormData.email.length<= 3 || FormData.password.length <= 5 || loading1"> LOG IN </v-btn>
+                <v-btn class="btn1" @click="login" :loading="loading1"
+                    :disabled="FormData.email.length <= 3 || FormData.password.length <= 5 || loading1"> LOG IN </v-btn>
                 <a class="llink" href="#">Forget password ?</a>
                 <div class="exx">
-                    <button class="btn2" > <img src="../assets/google-icon.svg" alt="logo of google "> Continue with google</button>
+                    <button class="btn2"> <img src="../assets/google-icon.svg" alt="logo of google "> Continue with
+                        google</button>
                 </div>
             </div>
             <div class="rout">
-                <router-link  class="okk" to="/registration">REGISTRATION</router-link>
+                <router-link class="okk" to="/registration">REGISTRATION</router-link>
             </div>
         </div>
 
@@ -35,40 +37,41 @@
 
 <script>
 
-export default{
-    data(){
+export default {
+    data() {
         return {
-            loading1: false, 
-            show1 : false ,
-            FormData:{
+            loading1: false,
+            show1: false,
+            FormData: {
                 email: '',
-                password:'',
+                password: '',
                 returnSecureToken: true
             },
-            errors:{
+            errors: {
                 email: '',
-                password:'',
+                password: '',
             },
         }
     },
-    methods:{
+    methods: {
         async login() {
             this.loading1 = true
-            if (this.FormData.email.length && this.FormData.password.length) {  
+            if (this.FormData.email.length && this.FormData.password.length) {
                 let options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type' : 'application/json'
-                },
-                body:JSON.stringify(this.FormData)
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.FormData)
                 }
-                const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${import.meta.env.VITE_FIREBASE_API_KEY}`, options)    
+                const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${import.meta.env.VITE_FIREBASE_API_KEY}`, options)
                 const res = await response.json()
                 console.log(res);
+                console.log(this.$router);
                 if (response.ok) {
                     localStorage.setItem('user', JSON.stringify(res))
-                    this.$router.push({path: '/private'})
-                }else{
+                    // this.$router.push({ path: '/user/1' })
+                } else {
                     this.$notify({
                         title: res.error.message,
                         type: 'error'
@@ -76,7 +79,6 @@ export default{
                 }
             }
             this.loading1 = false
-            
         },
     },
 }
@@ -84,76 +86,85 @@ export default{
 
 
 <style scoped>
-.chinp{
+.chinp {
     position: relative;
-    bottom: -5px ;
+    bottom: -5px;
 }
-.show{
+
+.show {
     display: flex;
     align-items: center;
-    gap: 5px ;
+    gap: 5px;
 }
-.llink{
+
+.llink {
     text-decoration: none;
     width: 100%;
-    color: black ;
-    margin: 10px ;
+    color: black;
+    margin: 10px;
     text-align: center;
-    border-top: 1px solid black ;
-    padding-top: 10px ;
+    border-top: 1px solid black;
+    padding-top: 10px;
 }
+
 .okk {
-  position: relative;
+    position: relative;
 }
 
 .okk::before {
-  content: '';
-  position: absolute;
-  top: -2px;
-  left: 0;
-  right: 0;
-  height: 2px;
-  background-color: #985ACE;
-  transform-origin: bottom right;
-  transform: scaleX(0);
-  transition: transform 0.5s ease;
+    content: '';
+    position: absolute;
+    top: -2px;
+    left: 0;
+    right: 0;
+    height: 2px;
+    background-color: #985ACE;
+    transform-origin: bottom right;
+    transform: scaleX(0);
+    transition: transform 0.5s ease;
 }
 
 .okk:hover::before {
-  transform-origin: bottom left;
-  transform: scaleX(1);
+    transform-origin: bottom left;
+    transform: scaleX(1);
 }
+
 .rout a {
     text-decoration: none;
-    color: black ;
+    color: black;
     text-transform: uppercase;
-    font-family:"Gill Sans", sans-serif ;
+    font-family: "Gill Sans", sans-serif;
 }
-.rout{
-    padding-top: 10px ;
+
+.rout {
+    padding-top: 10px;
 }
-.exx{
+
+.exx {
     width: 100%;
 }
-.btn-wrap{
+
+.btn-wrap {
     width: 70%;
-    border-bottom: 1px solid black ;
+    border-bottom: 1px solid black;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 }
-.btn2{
+
+.btn2 {
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 14px ;
+    gap: 14px;
     background: none;
     border: 1px solid #985ACE;
 }
-.btn1{
+
+.btn1 {
     background: #985ACE;
-    color: white ;
+    color: white;
     text-transform: uppercase;
     font-family: "Gill Sans", sans-serif;
     font-weight: 350;
@@ -162,55 +173,64 @@ export default{
 }
 
 .btn2 img {
-    width: 20px ;
+    width: 20px;
 }
-.btn1 , .btn2 {
+
+.btn1,
+.btn2 {
     /* border: none ; */
     width: 100%;
-    padding: 10px 0 ;
-    margin-bottom: 10px ;
-    border-radius: 4px ;
-} 
-.inp-wrap input {
-    height: 32px ;
-    padding: 0 10px ;
-    border: none ;
-    background: #F8F1FF;
-    margin-bottom: 10px ;
-    border-radius: 6px  ;
+    padding: 10px 0;
+    margin-bottom: 10px;
+    border-radius: 4px;
 }
-.inp-wrap{
-    display: flex ;
-    flex-direction: column ;
+
+.inp-wrap input {
+    height: 32px;
+    padding: 0 10px;
+    border: none;
+    background: #F8F1FF;
+    margin-bottom: 10px;
+    border-radius: 6px;
+}
+
+.inp-wrap {
+    display: flex;
+    flex-direction: column;
     width: 70%;
 }
-.main-wrapper{
+
+.main-wrapper {
     background: #985ACE;
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
 }
-.form-wrap{
+
+.form-wrap {
     background: white;
-    height: 60%; 
+    height: 60%;
     width: 30%;
     display: flex;
     flex-direction: column;
     /* justify-content: center; */
     align-items: center;
-    border-radius: 20px ;
+    border-radius: 20px;
     -webkit-box-shadow: 29px -31px 87px 0px rgba(34, 60, 80, 0.2);
     -moz-box-shadow: 29px -31px 87px 0px rgba(34, 60, 80, 0.2);
     box-shadow: 29px -31px 87px 0px rgba(34, 60, 80, 0.2);
 }
-label{
+
+label {
     color: rgba(0, 0, 0, 0.567);
 }
-.nammm{
-    padding: 30px 0 25px 0   ;
+
+.nammm {
+    padding: 30px 0 25px 0;
 }
-.nammm h2{
+
+.nammm h2 {
     text-transform: uppercase;
     font-family: "Gill Sans", sans-serif;
     font-weight: 600;
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -6,41 +6,42 @@
                 <h2>Registration</h2>
             </div>
             <form class="double-wrap" @submit.prevent="register">
-            <div class="inp-wrap">
-                <!-- <label  for="inp1">First Name</label>
+                <div class="inp-wrap">
+                    <!-- <label  for="inp1">First Name</label>
                 <input id="inp1"  v-model="hello" type="text" name="inp1" required>
                 <label  for="inp2">Last Name</label>
                 <input id="inp2"  v-model="hello" type="text"> -->
-                <label  for="inp3">E-mail</label>
-                <input id="inp3"  v-model="formData.email" type="email" required>
-                <p v-if="errors.email.length" >{{ errors.email }}</p>
-                <label  for="inp4">Password"</label>
-                <input id="inp4"  required v-model="formData.password" :type="show2 ? 'text' : 'password'" >
-                <p v-if="errors.password.length" >{{ errors.password }}</p>
-            </div>
-            <div class="show">
-                <input @change="show2 = !show2" class="chinp" type="checkbox">
-                <label>Show password</label>
-            </div>
-            <div class="btn-wrap">
-                <button class="btn1" >Registration</button>
-                <div class="exx">
-                    <button class="btn2" > <img src="../assets/google-icon.svg" alt="logo of google "> Continue with google</button>
+                    <label for="inp3">E-mail</label>
+                    <input id="inp3" v-model="formData.email" type="email" required>
+                    <p v-if="errors.email.length">{{ errors.email }}</p>
+                    <label for="inp4">Password"</label>
+                    <input id="inp4" required v-model="formData.password" :type="show2 ? 'text' : 'password'">
+                    <p v-if="errors.password.length">{{ errors.password }}</p>
                 </div>
-            </div>
-            <div class="rout">
-                <router-link  class="okk" to="/login" >Log in </router-link>
-            </div>
+                <div class="show">
+                    <input @change="show2 = !show2" class="chinp" type="checkbox">
+                    <label>Show password</label>
+                </div>
+                <div class="btn-wrap">
+                    <button class="btn1">Registration</button>
+                    <div class="exx">
+                        <button class="btn2"> <img src="../assets/google-icon.svg" alt="logo of google "> Continue with
+                            google</button>
+                    </div>
+                </div>
+                <div class="rout">
+                    <router-link class="okk" to="/login">Log in </router-link>
+                </div>
             </form>
         </div>
     </div>
 </template>
 
 <script>
-export default{
+export default {
     data() {
-        return{
-            show2 : false ,
+        return {
+            show2: false,
             formData: {
                 email: '',
                 password: '',
@@ -53,21 +54,21 @@ export default{
         }
     },
     methods: {
-        async register(){
-        const URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${import.meta.env.VITE_FIREBASE_API_KEY}`
-        const OPTIONS = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+        async register() {
+            const URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${import.meta.env.VITE_FIREBASE_API_KEY}`
+            const OPTIONS = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
                 body: JSON.stringify(this.formData)
-        }
+            }
             const response = await fetch(URL, OPTIONS)
             const data = await response.json()
-            if(response.ok) {
+            if (response.ok) {
                 localStorage.setItem('user', data)
-                this.$router.push({path: '/private'})
-            }else {
+                this.$router.push({ path: '/private' })
+            } else {
                 switch (data.error.message) {
                     case 'INVALID_EMAIL':
                         this.errors.email = 'Неверный email'
@@ -80,8 +81,8 @@ export default{
                         break
                     default:
                         this.$notify({
-                        title: data.error.message,
-                        type: 'error'
+                            title: data.error.message,
+                            type: 'error'
                         });
                 }
             }
@@ -91,124 +92,141 @@ export default{
 </script>
 
 <style scoped>
-form{
+form {
     width: 100%;
 }
+
 .okk {
-  position: relative;
+    position: relative;
 }
 
 .okk::before {
-  content: '';
-  position: absolute;
-  top: -2px;
-  left: 0;
-  right: 0;
-  height: 2px;
-  background-color: #985ACE;
-  transform-origin: bottom right;
-  transform: scaleX(0);
-  transition: transform 0.5s ease;
+    content: '';
+    position: absolute;
+    top: -2px;
+    left: 0;
+    right: 0;
+    height: 2px;
+    background-color: #985ACE;
+    transform-origin: bottom right;
+    transform: scaleX(0);
+    transition: transform 0.5s ease;
 }
-.double-wrap{
+
+.double-wrap {
     display: flex;
     flex-direction: column;
-    justify-content: center;    
+    justify-content: center;
     align-items: center;
 }
+
 .okk:hover::before {
-  transform-origin: bottom left;
-  transform: scaleX(1);
+    transform-origin: bottom left;
+    transform: scaleX(1);
 }
+
 .rout a {
     text-decoration: none;
-    color: black ;
+    color: black;
     text-transform: uppercase;
-    font-family:"Gill Sans", sans-serif ;
+    font-family: "Gill Sans", sans-serif;
 }
-.rout{
-    padding-top: 10px ;
+
+.rout {
+    padding-top: 10px;
 }
-.exx{
+
+.exx {
     width: 100%;
 }
-.btn-wrap{
+
+.btn-wrap {
     width: 70%;
-    border-bottom: 1px solid black ;
+    border-bottom: 1px solid black;
     margin: 15px 0;
 }
-.btn2{
+
+.btn2 {
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 14px ;
+    gap: 14px;
     background: none;
     border: 1px solid #985ACE;
 }
-.btn1{
+
+.btn1 {
     background: #985ACE;
-    color: white ;
+    color: white;
     text-transform: uppercase;
     font-family: "Gill Sans", sans-serif;
     font-weight: 350;
-    border-bottom: 1px solid black ;
+    border-bottom: 1px solid black;
     border: 1px solid #985ACE;
 }
 
 .btn2 img {
-    width: 20px ;
+    width: 20px;
 }
-.btn1 , .btn2 {
+
+.btn1,
+.btn2 {
     /* border: none ; */
     width: 100%;
-    padding: 10px 0 ;
-    margin-bottom: 10px ;
-    border-radius: 4px ;
-} 
-.inp-wrap input {
-    height: 32px ;
-    padding: 0 10px ;
-    border: none ;
-    background: #F8F1FF;
-    margin-bottom: 10px ;
-    border-radius: 6px  ;
+    padding: 10px 0;
+    margin-bottom: 10px;
+    border-radius: 4px;
 }
-.inp-wrap{
-    display: flex ;
-    flex-direction: column ;
+
+.inp-wrap input {
+    height: 32px;
+    padding: 0 10px;
+    border: none;
+    background: #F8F1FF;
+    margin-bottom: 10px;
+    border-radius: 6px;
+}
+
+.inp-wrap {
+    display: flex;
+    flex-direction: column;
     width: 70%;
 }
-.main-wrapper{
+
+.main-wrapper {
     background: #985ACE;
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
 }
-.form-wrap{
+
+.form-wrap {
     background: white;
-    height: 55%; 
+    height: 55%;
     width: 30%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 
-    border-radius: 20px ;
+    border-radius: 20px;
     -webkit-box-shadow: 29px -31px 87px 0px rgba(34, 60, 80, 0.2);
     -moz-box-shadow: 29px -31px 87px 0px rgba(34, 60, 80, 0.2);
     box-shadow: 29px -31px 87px 0px rgba(34, 60, 80, 0.2);
 }
-label{
+
+label {
     color: rgba(0, 0, 0, 0.567);
 }
-.nammm{
-    padding: 30px 0 25px 0   ;
+
+.nammm {
+    padding: 30px 0 25px 0;
 }
-.nammm h2{
+
+.nammm h2 {
     text-transform: uppercase;
     font-family: "Gill Sans", sans-serif;
     font-weight: 600;
 
-}
-</style>
+}</style>
